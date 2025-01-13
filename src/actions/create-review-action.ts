2894cc0c56bb42ac3034a7fd24a978cf90aef35e
Joowon <kimjw0913@gmail.com>
2025-01-13 15:22:38 +0900

    클라이언트 컴포넌트에서 서버액션 실습

--- src/actions/create-review-action.ts
+++ src/actions/create-review-action.ts
@@ -1,27 +1,38 @@
 "use server";
 
-import { revalidatePath, revalidateTag } from "next/cache";
+import { delay } from "@/util/delay";
+import { revalidateTag } from "next/cache";
 
-export async function createReviewAction(formData: FormData) {
+export async function createReviewAction(
+  _: any, 
+  formData: FormData
+) {
   const bookId = formData.get("bookId")?.toString();
   const content = formData.get("content")?.toString();
   const author = formData.get("author")?.toString();
 
   console.log(bookId, content, author);
 
-  if(!content || !author) {
-    return;
+  if(!bookId || !content || !author) {
+    return {
+      status: false,
+      error: "리뷰 내용과 작성자를 입력해주세요"
+    }
   }
 
   try {
+    await delay(2000);
     const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`, 
+      `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review/1`, 
       {
           method: "POST",
           body: JSON.stringify({ bookId, content, author })
       }
     );
-    console.log(response.status);
+  
+    if(!response.ok) {
+      throw new Error(response.statusText);
+    }
 
     // 1. 특정 주소의 해당하는 페이지만 재검증 
     // revalidatePath(`/book/${bookId}`);
@@ -37,9 +48,14 @@ export async function createReviewAction(formData: FormData) {
 
     // 5. 태그 기준, 데이터 캐시 재검증
     revalidateTag(`review-${bookId}`);
-    return response.json();
+    return {
+      status: true,
+      error: "",
+    };
   } catch (err) {
-    console.error(err);
-    return;
+    return {
+      status: false,
+      error: `리뷰 저장에 실패했습니다: ${err}`,
+    };
   }
 }
